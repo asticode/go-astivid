b0a32492af37230170562a38f74fbc80e0438812
Quentin Renard <contact@asticode.com>
2018-02-27 11:42:48 +0100

    Now merging results in one chart when several paths are analyzed

--- message.go
+++ message.go
@@ -3,9 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"path/filepath"
-	"strconv"
 
 	"github.com/asticode/go-astichartjs"
 	"github.com/asticode/go-astiffprobe"
@@ -26,6 +24,38 @@ func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (_ interface{}
 	return
 }
 
+type chartColors struct {
+	Background, Border string
+}
+
+func allChartColors() []*chartColors {
+	return []*chartColors{
+		{Background: astichartjs.ChartBackgroundColorBlue, Border: astichartjs.ChartBorderColorBlue},
+		{Background: astichartjs.ChartBackgroundColorGreen, Border: astichartjs.ChartBorderColorGreen},
+		{Background: astichartjs.ChartBackgroundColorRed, Border: astichartjs.ChartBorderColorRed},
+		{Background: astichartjs.ChartBackgroundColorYellow, Border: astichartjs.ChartBorderColorYellow},
+		{Background: astichartjs.ChartBackgroundColorPurple, Border: astichartjs.ChartBorderColorPurple},
+		{Background: astichartjs.ChartBackgroundColorOrange, Border: astichartjs.ChartBorderColorOrange},
+	}
+}
+
+type chartColorPicker struct {
+	colors []*chartColors
+}
+
+func newChartColorPicker() *chartColorPicker {
+	return &chartColorPicker{colors: allChartColors()}
+}
+
+func (p *chartColorPicker) next() (c *chartColors) {
+	if len(p.colors) > 0 {
+		c = p.colors[0]
+		p.colors = p.colors[1:]
+		return
+	}
+	return
+}
+
 // handleGetFrames handles the "get.frames" event
 func handleGetFrames(i bootstrap.MessageIn) (payload interface{}, err error) {
 	// Decode input
@@ -35,8 +65,41 @@ func handleGetFrames(i bootstrap.MessageIn) (payload interface{}, err error) {
 		return
 	}
 
+	// Create charts
+	// TODO Increase title font size
+	// TODO Resize doesn't work on MacOSX
+	// TODO Disable border on btn click
+	var cs = []astichartjs.Chart{{
+		Data: &astichartjs.Data{},
+		Options: &astichartjs.Options{
+			Scales: &astichartjs.Scales{
+				XAxes: []astichartjs.Axis{
+					{
+						Position: astichartjs.ChartAxisPositionsBottom,
+						ScaleLabel: &astichartjs.ScaleLabel{
+							Display:     astiptr.Bool(true),
+							LabelString: "Timestamp (s)",
+						},
+						Type: astichartjs.ChartAxisTypesLinear,
+					},
+				},
+				YAxes: []astichartjs.Axis{
+					{
+						ScaleLabel: &astichartjs.ScaleLabel{
+							Display:     astiptr.Bool(true),
+							LabelString: "Bitrate (kb/s)",
+						},
+					},
+				},
+			},
+		},
+		Type: astichartjs.ChartTypeLine,
+	}}
+
+	// Create color picker
+	cp := newChartColorPicker()
+
 	// Loop through paths
-	var cs = []astichartjs.Chart{}
 	for _, p := range ps {
 		// Retrieve streams
 		var ss []astiffprobe.Stream
@@ -54,45 +117,6 @@ func handleGetFrames(i bootstrap.MessageIn) (payload interface{}, err error) {
 				continue
 			}
 
-			// Init chart
-			// TODO Increase title font size
-			// TODO Resize doesn't work on MacOSX
-			// TODO Disable border on btn click
-			var c = astichartjs.Chart{
-				Data: &astichartjs.Data{},
-				Options: &astichartjs.Options{
-					Scales: &astichartjs.Scales{
-						XAxes: []astichartjs.Axis{
-							{
-								Position: astichartjs.ChartAxisPositionsBottom,
-								ScaleLabel: &astichartjs.ScaleLabel{
-									Display:     astiptr.Bool(true),
-									LabelString: "Timestamp (s)",
-								},
-								Type: astichartjs.ChartAxisTypesLinear,
-							},
-						},
-						YAxes: []astichartjs.Axis{
-							{
-								ScaleLabel: &astichartjs.ScaleLabel{
-									Display:     astiptr.Bool(true),
-									LabelString: "Bitrate (kb/s)",
-								},
-							},
-						},
-					},
-					Title: &astichartjs.Title{Display: astiptr.Bool(true)},
-				},
-				Type: astichartjs.ChartTypeLine,
-			}
-
-			// Get chart title
-			c.Options.Title.Text = filepath.Base(p) + " - "
-			if s.Bitrate > 0 {
-				c.Options.Title.Text += strconv.Itoa(int(s.Bitrate/1024)) + "kb - "
-			}
-			c.Options.Title.Text += fmt.Sprintf("%dx%d", s.Width, s.Height)
-
 			// Retrieve frames
 			var fs []astiffprobe.Frame
 			astilog.Debugf("Retrieving frames of stream %d of %s", s.Index, p)
@@ -101,14 +125,20 @@ func handleGetFrames(i bootstrap.MessageIn) (payload interface{}, err error) {
 				return
 			}
 
-			// Loop through frames
-			var ds = map[string]*astichartjs.Dataset{
-				"avg": {
-					BackgroundColor: astichartjs.ChartBackgroundColorGreen,
-					BorderColor:     astichartjs.ChartBorderColorGreen,
-					Label:           "Average per GOP",
-				},
+			// Create colors
+			clr := cp.next()
+			if clr == nil {
+				continue
 			}
+
+			// Create dataset
+			var d = &astichartjs.Dataset{
+				BackgroundColor: clr.Background,
+				BorderColor:     clr.Border,
+				Label:           filepath.Base(p),
+			}
+
+			// Loop through frames
 			var vs []astichartjs.DataPoint
 			for _, f := range fs {
 				// Compute average
@@ -117,7 +147,7 @@ func handleGetFrames(i bootstrap.MessageIn) (payload interface{}, err error) {
 					for _, dp := range vs {
 						sum += dp.Y
 					}
-					ds["avg"].Data = append(ds["avg"].Data, astichartjs.DataPoint{
+					d.Data = append(d.Data, astichartjs.DataPoint{
 						X: vs[0].X,
 						Y: sum / float64(len(vs)),
 					})
@@ -134,13 +164,11 @@ func handleGetFrames(i bootstrap.MessageIn) (payload interface{}, err error) {
 				}
 			}
 
-			// Loop through datasets
-			for _, d := range ds {
-				c.Data.Datasets = append(c.Data.Datasets, *d)
-			}
+			// Append dataset
+			cs[0].Data.Datasets = append(cs[0].Data.Datasets, *d)
 
-			// Add chart
-			cs = append(cs, c)
+			// We only process one stream per path
+			break
 		}
 	}
 	payload = cs
